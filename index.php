24b4a7a253f6f380b22349121172b75690b1933f
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-09-21 23:47:27 +0800

    added str_replace stringa

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
     <title>Document</title>
 </head>
 <body>
+    <?php 
+    $paragr = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam, suscipit provident iusto consequatur natus, sapiente, quia laboriosam harum dolores dolorem aliquam officiis non enim consectetur dignissimos voluptate fuga vel.';
+    $utente = $_GET['quary'];
+    
+    echo $paragr . '<br>';
+
+    echo strlen($paragr) . '<br>';
+    //echo strpos($paragr, $utente);
+    echo str_replace($utente, '***',$paragr);
+    ?>
     
 </body>
 </html>
