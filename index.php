2728083b7cc0705e6f58ec66c4630f4ced73bc11
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-09-25 00:43:09 +0800

    added correct version

--- index.php
+++ index.php
@@ -1,21 +1,46 @@
+<?php
+/*
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+*/
+
+
+$paragr = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam, suscipit provident iusto consequatur natus, sapiente, quia laboriosam harum dolores dolorem aliquam officiis non enim consectetur dignissimos voluptate fuga vel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga soluta? Eaque fugiat totam facilis alias sapiente eius dolore velit culpa accusantium? Laboriosam beatae quidem, cumque accusamus saepe iure quisquam?';
+
+$paragr_lenght = strlen($paragr);
+
+
+$badword = $_GET['word'];
+
+$censured = str_replace($badword, '***', $paragr);
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Badword</title>
 </head>
-<body>
-    <?php 
-    $paragr = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aperiam, suscipit provident iusto consequatur natus, sapiente, quia laboriosam harum dolores dolorem aliquam officiis non enim consectetur dignissimos voluptate fuga vel.';
-    $utente = $_GET['quary'];
-    
-    echo $paragr . '<br>';
 
-    echo strlen($paragr) . '<br>';
-    echo str_replace($utente, '***',$paragr);
-    ?>
-    
+<body>
+    <p>
+        <?php echo $paragr ?>
+    </p>
+    <h6>La lunghezza del paragrafo è di: <?php echo $paragr_lenght; ?> caratteri</h6>
+    <hr>
+    <p>
+        <?php echo $censured ?>
+    </p>
+    <h6>La lunghezza del paragrafo censurato è di: <?php echo strlen($censured) ?>caratteri</h6>
 </body>
+
 </html>
